b57544878050e8ea639304aeb8fb758ca58b8484
Gustavo Orelio <oreliogustavo@gmail.com>
2022-05-17 22:37:41 -0300

    Ajustando o acoplamento do projeto

--- loja_virtual/lib/loja_virtual.dart
+++ loja_virtual/lib/loja_virtual.dart
@@ -6,11 +6,12 @@ void show() {
   print("O valor a ser pago junto com juros é $valorTotalComJuros");
   var valorTotalComDesconto = valorComDesconto(120.00, 10);
   print("O valor com desconto é $valorTotalComDesconto");
-  geradorNumerosBingo();
+  var bingo = geradorNumerosBingo();
+  print("Os numeros sorteados para o bingo são $bingo");
   var valorFinal = valorFinalCompra(120, 5);
   print("Voce vai gastar um total de $valorFinal");
   var valorFrete = calcularValorFrete(valorCompra: 400);
-  print("$valorFrete");
+  print(valorFrete);
 }
 
 void validarValor(double valor) {
@@ -36,11 +37,10 @@ double valorComDesconto(double valorProduto, double percentualDesconto) {
 
 /*A função a seguir foi criado pensando em gerar numeros aleatorios para um bingo na qual os 
 clientes que tiverem todos os numeros na cartela terão um desconto maior na proxima compra*/
-void geradorNumerosBingo() {
+String geradorNumerosBingo() {
   var gerar = Random();
   var bingo = new List.generate(12, (_) => gerar.nextInt(100));
-  print(
-      "Foi gerado os numeros a seguir para preencher a cartela de bingo para obter um desconto maior na proxima compra:  $bingo");
+  return "$bingo";
 }
 
 double valorFinalCompra(double valorProduto,
